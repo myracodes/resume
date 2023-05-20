3e86a0a7c1bf219b033962b8ab0a48437050749a
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2023-05-20 13:19:53 +0200

    fix: build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ProfessionalXpComponent } from './components/professional-xp/professional-xp.component';
-import { EducationComponent } from './components/education/education.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { ActivitiesComponent } from './components/activities/activities.component';
-import { TitleComponent } from './components/atoms/title/title.component';
-import { CertificationsComponent } from './components/certifications/certifications.component';
-import { ProSkillsComponent } from './components/pro-skills/pro-skills.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { EducationComponent } from "./components/education/education.component";
+import { SkillsComponent } from "./components/skills/skills.component";
+import { ActivitiesComponent } from "./components/activities/activities.component";
+import { TitleComponent } from "./components/atoms/title/title.component";
+import { CertificationsComponent } from "./components/certifications/certifications.component";
+import { ProSkillsComponent } from "./components/pro-skills/pro-skills.component";
+import { ProfessionalXpComponent } from "./components/professional-xp/professional-xp.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ProfessionalXpComponent,
-    EducationComponent,
-    SkillsComponent,
-    ActivitiesComponent,
-    TitleComponent,
-    CertificationsComponent,
-    ProSkillsComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        ProfessionalXpComponent,
+        EducationComponent,
+        SkillsComponent,
+        ActivitiesComponent,
+        TitleComponent,
+        CertificationsComponent,
+        ProSkillsComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
