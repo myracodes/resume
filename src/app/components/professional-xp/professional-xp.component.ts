a4eba1c434f3d9e14ee52f3963ca0bfbea7374c2
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2023-06-18 15:48:07 +0200

    hotfix: wrong build in prod

--- src/app/components/professional-xp/professional-xp.component.ts
+++ src/app/components/professional-xp/professional-xp.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { IDetailsDialog } from "src/app/interfaces/DetailsDialog.interface";
 
 @Component({
-  selector: 'app-professional-xp',
-  templateUrl: './professional-xp.component.html',
-  styleUrls: ['./professional-xp.component.scss']
+    selector: "app-professional-xp",
+    templateUrl: "./professional-xp.component.html",
+    styleUrls: ["./professional-xp.component.scss"],
 })
 export class ProfessionalXpComponent {
-
+    detailsAvanade: IDetailsDialog = {
+        header: "Chez Avanade",
+        content: "plus de détails sur mon xp chez Ava",
+        footer: "Footer",
+    };
 }
