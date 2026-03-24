185faac40e21f42cc390e6f95d4c30414b390e12
Myriam MIRA <myriam@cap-collectif.com>
2026-03-24 19:33:30 +0100

    feat: create reusable button component

---
+++ src/app/components/atoms/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: "app-button",
+    templateUrl: "./button.component.html",
+    styleUrls: ["./button.component.scss"],
+})
+export class ButtonComponent {
+    @Input() label: string = "";
+    @Input() type: "button" | "submit" | "reset" = "button";
+    @Input() disabled: boolean = false;
+    @Input() variant: "primary" | "secondary" | "ghost" = "primary";
+    @Input() ariaLabel: string = "";
+    @Input() buttonId: string = "";
+}
--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -1,15 +1,16 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { DialogModule } from "primeng/dialog";
+import { ButtonComponent } from "../../components/atoms/button/button.component";
 
 const ANGULAR_MODULE = [BrowserAnimationsModule];
 
 const PRIMENG = [DialogModule];
 
 @NgModule({
-    declarations: [],
+    declarations: [ButtonComponent],
     imports: [ANGULAR_MODULE, PRIMENG],
-    exports: [ANGULAR_MODULE, PRIMENG],
+    exports: [ANGULAR_MODULE, PRIMENG, ButtonComponent],
     providers: [],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
