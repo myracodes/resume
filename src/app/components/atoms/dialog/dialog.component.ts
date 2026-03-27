cfcf988bc3bdf94c7d75d13f088b923591a5ab5e
Myriam MIRA <myriam@cap-collectif.com>
2026-03-27 13:49:27 +0100

    fix: dialog component breaking change Styles were not applied anymore due to a primeNG version upgrade. This commit fixes that and refactors variables/global styles and mixins for clarity

--- src/app/components/atoms/dialog/dialog.component.ts
+++ src/app/components/atoms/dialog/dialog.component.ts
@@ -1,17 +1,17 @@
-import { Component, ElementRef, HostListener, Input } from '@angular/core';
-import { closeOnClickOutside } from 'src/app/shared/utils/mouseEvent.utils';
+import { Component, ElementRef, HostListener, Input } from "@angular/core";
+import { closeOnClickOutside } from "src/app/shared/utils/mouseEvent.utils";
 
 @Component({
-    selector: 'app-dialog',
-    templateUrl: './dialog.component.html',
-    styleUrls: ['./dialog.component.scss'],
-    standalone: false
+    selector: "app-dialog",
+    templateUrl: "./dialog.component.html",
+    styleUrls: ["./dialog.component.scss"],
+    standalone: false,
 })
 export class DialogComponent {
     elementRef: any;
     isVisible: boolean = false;
 
-    @Input() buttonLabel: string = 'En savoir plus +';
+    @Input() buttonLabel: string = "En savoir plus +";
 
     constructor(myElement: ElementRef) {
         this.elementRef = myElement;
@@ -21,7 +21,7 @@ export class DialogComponent {
         this.isVisible = true;
     }
 
-    @HostListener('document:mousedown', ['$event']) clickFct(event: any) {
+    @HostListener("document:mousedown", ["$event"]) clickFct(event: any) {
         const close = closeOnClickOutside(event, this.elementRef.nativeElement);
         if (close) {
             this.isVisible = false;
